72e80760e385f995c42c67457a72d4c6d5c0f58d
clearcmos <noreply>
2026-04-07 01:52:15 -0400

    fix: improve cpush output formatting and truncate long results

--- config/shell/aliases.sh
+++ config/shell/aliases.sh
@@ -21,17 +21,18 @@ cpush() {
       if .type == "assistant" then
         [.message.content[]? |
           if .type == "tool_use" then
-            "> \(.name): \(.input.command // .input.file_path // "")\n"
+            "  \(.input.command // .input.file_path // .name)\n"
           elif .type == "text" then
-            "\(.text)\n"
+            "\n\(.text)\n"
           else empty end
         ] | join("")
       elif .type == "user" and .tool_use_result then
-        if (.tool_use_result.stdout // "") != "" then
-          "  \(.tool_use_result.stdout | split("\n") | join("\n  "))\n"
+        (.tool_use_result.stdout // "") |
+        if . != "" then
+          split("\n") | if length > 6 then .[:3] + ["  ...(\(length - 3) more lines)"] else . end |
+          map("    \(.)") | join("\n") + "\n"
         else empty end
-      elif .type == "result" then
-        "\n\(.result)\n"
+      elif .type == "result" then empty
       else empty end
     '
 }
